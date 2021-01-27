a0d4f1287ebd893e030e1392cec9319186645141
Grzegorz Listwan <listwan94@gmail.com>
2021-01-27 16:02:48 +0100

    Create menu

---
+++ to_do_list.py
@@ -0,0 +1,11 @@
+print()
+print("Witamy w aplikacji To Do List")
+print()
+print("1. Wyświtl wszytkie zadania")
+print("2. Dodaj zadanie")
+print("3. Usuń zadanie")
+print("4. Wyjdź")
+print()
+user_choice = int(input("Wybierz opcję z menu: "))
+
+
