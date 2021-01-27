b7b372df412c6ee124b8ed2f5ef2bbc76b89ffb7
Grzegorz Listwan <listwan94@gmail.com>
2021-01-27 16:17:59 +0100

    Add function add task

--- to_do_list.py
+++ to_do_list.py
@@ -4,17 +4,25 @@ tasks.append("Pierwsze zadanie")
 tasks.append("Drugie zadanie")
 tasks.append("Trzecie  zadanie")
 tasks.append("Czwarte zadanie")
+
 def show_tasks():
     for task in tasks:
         print(task)
+
+def add_task():
+    user_task = input("Podaj treść zadania: ")
+    tasks.append(user_task)
+
+
 print()
 print("Witamy w aplikacji To Do List")
 print()
 while user_choice != 4:
     if user_choice == 1:
         show_tasks()
+        print()
     elif user_choice == 2:
-        print("Dodaj zadania")
+        add_task()
     elif user_choice == 3:
         print("Usuń zadanie")
     else:
@@ -26,5 +34,6 @@ while user_choice != 4:
     print("4. Wyjdź")
     print()
     user_choice = int(input("Wybierz opcję z menu: "))
+    print()
 
 
