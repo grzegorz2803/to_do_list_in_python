9f93d471e5e3858e76a26d4700f33f22ea369b8a
Grzegorz Listwan <listwan94@gmail.com>
2021-01-27 16:07:05 +0100

    Modification menu

--- to_do_list.py
+++ to_do_list.py
@@ -1,11 +1,13 @@
+user_choice = -1
 print()
 print("Witamy w aplikacji To Do List")
 print()
-print("1. Wyświtl wszytkie zadania")
-print("2. Dodaj zadanie")
-print("3. Usuń zadanie")
-print("4. Wyjdź")
-print()
-user_choice = int(input("Wybierz opcję z menu: "))
+while user_choice != 4:
+    print("1. Wyświtl wszytkie zadania")
+    print("2. Dodaj zadanie")
+    print("3. Usuń zadanie")
+    print("4. Wyjdź")
+    print()
+    user_choice = int(input("Wybierz opcję z menu: "))
 
 
