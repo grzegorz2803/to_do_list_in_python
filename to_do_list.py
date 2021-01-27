f6e0eeae1a8ddd58e02578c8507088d0377568ac
Grzegorz Listwan <listwan94@gmail.com>
2021-01-27 16:11:23 +0100

    Add menu logic

--- to_do_list.py
+++ to_do_list.py
@@ -1,8 +1,23 @@
 user_choice = -1
+tasks = []
+tasks.append("Pierwsze zadanie")
+tasks.append("Drugie zadanie")
+tasks.append("Trzecie  zadanie")
+tasks.append("Czwarte zadanie")
+
 print()
 print("Witamy w aplikacji To Do List")
 print()
 while user_choice != 4:
+    if user_choice == 1:
+        print("Wyświetl zadania")
+    elif user_choice == 2:
+        print("Dodaj zadania")
+    elif user_choice == 3:
+        print("Usuń zadanie")
+    else:
+        print("Błędna pozycja w menu")
+        
     print("1. Wyświtl wszytkie zadania")
     print("2. Dodaj zadanie")
     print("3. Usuń zadanie")
