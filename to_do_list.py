70f65b618ea8cdf9b81262ff98dcad3a7dfd2890
Grzegorz Listwan <listwan94@gmail.com>
2021-01-27 16:13:53 +0100

    Add function show_tasks, create tasks list  and add to tasks list sample values

--- to_do_list.py
+++ to_do_list.py
@@ -4,20 +4,22 @@ tasks.append("Pierwsze zadanie")
 tasks.append("Drugie zadanie")
 tasks.append("Trzecie  zadanie")
 tasks.append("Czwarte zadanie")
-
+def show_tasks():
+    for task in tasks:
+        print(task)
 print()
 print("Witamy w aplikacji To Do List")
 print()
 while user_choice != 4:
     if user_choice == 1:
-        print("Wyświetl zadania")
+        show_tasks()
     elif user_choice == 2:
         print("Dodaj zadania")
     elif user_choice == 3:
         print("Usuń zadanie")
     else:
         print("Błędna pozycja w menu")
-        
+
     print("1. Wyświtl wszytkie zadania")
     print("2. Dodaj zadanie")
     print("3. Usuń zadanie")
